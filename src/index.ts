70af0388c40aae759e9f1fe4966230f66b14f423
Nicolas Pearson <nic.s.pearson@gmail.com>
2018-09-02 14:38:02 +0200

    Added Index With Default Exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import SearchTerm from './models/internal/search-term.internal';
+import { ISearchQueryBuilderOptions } from './models/options/search-query-builder.options';
+import BaseRepository from './repositories/base.repository';
+import BaseService from './services/base.service';
+
+export { BaseRepository, BaseService, ISearchQueryBuilderOptions, SearchTerm };
